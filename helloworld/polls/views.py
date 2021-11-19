95668154e789be95cc073ddcbb848454612ca568
Facundo García Martoni <thunderxforce@gmail.com>
2021-11-19 17:56:53 -0300

    templates_de_django

--- helloworld/polls/views.py
+++ helloworld/polls/views.py
@@ -7,7 +7,7 @@ from .models import Question
 def index(request): 
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # output = ', '.join([q.question_text for q in latest_question_list])
-    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list}) 
 
 
 def detail(request, question_id):
