189ee419ed085f0d561a278a6f098e8b79c462ec
Facundo García Martoni <thunderxforce@gmail.com>
2021-11-19 17:56:21 -0300

    Templates de Django

--- helloworld/polls/views.py
+++ helloworld/polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse, response
+from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request): 
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
 
 
 def detail(request, question_id):
