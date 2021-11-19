c03dcee93a1edab00c447c0fa26a4cad5bde1e2f
Facundo García Martoni <thunderxforce@gmail.com>
2021-11-19 18:24:43 -0300

    elevando_el_error_404

--- helloworld/polls/views.py
+++ helloworld/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Question
@@ -11,7 +12,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
