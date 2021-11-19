dce34965bde497d205ce721eff3bae3ac0d7c487
Facundo García Martoni <thunderxforce@gmail.com>
2021-11-19 17:25:41 -0300

    creando_vistas_para_la_aplicacion

--- helloworld/polls/views.py
+++ helloworld/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
+
 
 def index(request): 
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+
+def vote(request, question_id): 
+    return HttpResponse(f"You're voting on question {question_id}")
