939fc8aaef5e7ca84c8f9f62403c3055b0edeeb9
Facundo García Martoni <thunderxforce@gmail.com>
2021-11-20 12:33:50 -0300

    formularios

--- helloworld/polls/views.py
+++ helloworld/polls/views.py
@@ -1,8 +1,9 @@
 from django.http.response import Http404
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request): 
@@ -17,8 +18,20 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id): 
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist): 
+        return render(request, "polls/detail.html", {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else: 
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
