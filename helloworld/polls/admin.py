db69c3689fce384e7f5dc72fdc031fa185309106
Facundo García Martoni <thunderxforce@gmail.com>
2021-11-21 16:04:00 -0300

    mejorando_el_admin_change_list

--- helloworld/polls/admin.py
+++ helloworld/polls/admin.py
@@ -9,5 +9,8 @@ class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
     inlines = [ChoiceInline]
     # This tells Django: “Choice objects are edited on the Question admin page. By default, provide enough fields for 3 choices.”
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
