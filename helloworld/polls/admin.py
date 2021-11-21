d99154a836c8143ed8daa9b7c3080be3733cf7ef
Facundo García Martoni <thunderxforce@gmail.com>
2021-11-21 15:26:41 -0300

    mejorando_el_admin_questions

--- helloworld/polls/admin.py
+++ helloworld/polls/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline): 
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiceInline]
+    # This tells Django: “Choice objects are edited on the Question admin page. By default, provide enough fields for 3 choices.”
+
+admin.site.register(Question, QuestionAdmin)
